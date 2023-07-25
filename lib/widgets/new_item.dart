6b67831a493176b0c0014c4892ff1c1bd0fbe5f0
brianmujjuni <brianmujjuni@engineer.com>
2023-07-25 19:38:24 +0300

    added post functionality

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shoppi_list/data/categories.dart';
 import 'package:shoppi_list/models/category.dart';
 import 'package:shoppi_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,14 +24,24 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https(
+        'flutterbeginnerforms-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      );
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity6': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
         ),
       );
+      // Navigator.of(context).pop();
     }
   }
 
