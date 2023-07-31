32d398bbf140e639fdd9ba2609365d42953780f4
brianmujjuni <brianmujjuni@engineer.com>
2023-07-31 17:10:57 +0300

    added null catch

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shoppi_list/data/categories.dart';
-//import 'package:shoppi_list/data/dummy_items.dart';
+
 import 'package:shoppi_list/models/grocery_item.dart';
 import 'package:shoppi_list/widgets/new_item.dart';
-import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -29,6 +29,7 @@ class _GroceryListState extends State<GroceryList> {
       'shopping-list.json',
     );
     final response = await http.get(url);
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -51,12 +52,19 @@ class _GroceryListState extends State<GroceryList> {
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
-        builder: (context) => const NewItem(),
+        builder: (ctx) => const NewItem(),
       ),
     );
-    _loadItems();
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   void _removeItem(GroceryItem item) {
@@ -67,14 +75,12 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(
-      child: Text('No items added yes'),
-    );
+    Widget content = const Center(child: Text('No items added yet.'));
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => Dismissible(
+        itemBuilder: (ctx, index) => Dismissible(
           onDismissed: (direction) {
             _removeItem(_groceryItems[index]);
           },
@@ -93,14 +99,15 @@ class _GroceryListState extends State<GroceryList> {
         ),
       );
     }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Grocery List'),
+        title: const Text('Your Groceries'),
         actions: [
           IconButton(
             onPressed: _addItem,
             icon: const Icon(Icons.add),
-          )
+          ),
         ],
       ),
       body: content,
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:shoppi_list/data/categories.dart';
 import 'package:shoppi_list/models/category.dart';
 import 'package:http/http.dart' as http;
+import 'package:shoppi_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -40,10 +41,19 @@ class _NewItemState extends State<NewItem> {
         ),
       );
 
+      final Map<String, dynamic> resData = json.decode(response.body);
+
       if (!context.mounted) {
         return;
       }
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
