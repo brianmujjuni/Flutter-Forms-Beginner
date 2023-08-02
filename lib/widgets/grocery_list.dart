a6939541710bbbbb094b02090739f7daeb337652
brianmujjuni <brianmujjuni@engineer.com>
2023-08-02 19:11:22 +0300

    added delete functionality

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -76,10 +76,22 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    final url = Uri.https(
+      'flutterbeginnerforms-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      //optional: show error message
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
